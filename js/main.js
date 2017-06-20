d74fc5a9680dc5eafde4927c445b4610a8c3a68d
pramod88s <pramod.somvanshi@gmail.com>
2017-06-20 16:46:37 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@
 const applicationServerPublicKey = 'BKuXoVOSp6LD7S0z4s0tPESi2cfjiVyBSE9SjwHISuWyEDk3yg_ivjNIX-QINEljHiNDl7Y78jEU1Z7nCKSIHBs';
 
 var domain = "web.tech.lahiru";
-var pushServerRegUrl = "http://localhost:9999/web-push-sender/register";
-var pushServerUnregUrl = "http://localhost:9999/web-push-sender/unregister";
+var pushServerRegUrl = "http://wap.indiatimes.com/web-push-sender/register";
+var pushServerUnregUrl = "http://wap.indiatimes.com/web-push-sender/unregister";
 var ua = window.navigator.userAgent,
 safariTxt = ua.indexOf ( "Safari" ),
 chrome = ua.indexOf ( "Chrome" ),
